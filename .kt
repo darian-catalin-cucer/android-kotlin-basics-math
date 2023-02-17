810d3e1800a844405583517698b396b8111d12f9
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-17 12:55:22 +0200

    Create .kt
    Explanation:
    
    Two variables x and y are defined with values 10 and 20, respectively.
    Basic math operations (addition, subtraction, multiplication, division, and modulus) are performed using these variables and the results are stored in separate variables.
    The results are printed to the console using string interpolation.

---
+++ .kt
@@ -0,0 +1,25 @@
+// Basic Math operations in Kotlin
+fun main() {
+    val x = 10
+    val y = 20
+
+    // Addition
+    val sum = x + y
+    println("Sum of $x and $y is $sum")
+
+    // Subtraction
+    val diff = y - x
+    println("Difference between $y and $x is $diff")
+
+    // Multiplication
+    val product = x * y
+    println("Product of $x and $y is $product")
+
+    // Division
+    val quotient = y / x
+    println("Quotient of $y and $x is $quotient")
+
+    // Modulus (remainder)
+    val remainder = y % x
+    println("Remainder of $y divided by $x is $remainder")
+}
